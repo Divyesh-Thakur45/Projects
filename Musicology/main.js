f12bce30c578f999e2e205c48ae20f69e4812b8b
Divyesh-Thakur45 <divyeshthakur370@gmail.com>
2024-10-08 16:12:42 +0530

    update

---
+++ Musicology/main.js
@@ -0,0 +1,44 @@
+
+$.fn.jQuerySimpleCounter = function( options ) {
+    var settings = $.extend({
+        start:  0,
+        end:    100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options );
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function() {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 1240,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 2000,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 1000,duration: 2000});
+// $('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
+
+
+
+  /* AUTHOR LINK */
+ $('.about-me-img').hover(function(){
+        $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+    }, function(){
+        $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+    });
+
+
+    $(document).ready(function(){
+        $(".bar").click(function(){
+            $("#navigation").toggle(600);
+        })
+    })
