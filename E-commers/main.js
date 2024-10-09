979b3c007dbce746c03a8813ab13dc5ad557d87e
Divyesh-Thakur45 <divyeshthakur370@gmail.com>
2024-10-09 09:20:56 +0530

    update

---
+++ E-commers/main.js
@@ -0,0 +1,499 @@
+// icone rotate
+$(document).ready(function(){
+  $(".sidenav-content").click(function(){
+      $(".hexa").toggleClass("active")
+  })
+})
+
+
+// navigatin
+$(document).ready(function(){
+    $(".menu").click(function(){
+        $("#side-navigation").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content").click(function(){
+        $(".sub-sidenav-content").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-commerece").click(function(){
+        $(".sub-sidenav-content-commerece").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-admine").click(function(){
+        $(".sub-sidenav-content-admine").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-custumer").click(function(){
+        $(".sub-sidenav-content-custumer").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-crm").click(function(){
+        $(".sub-sidenav-content-crm").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-project").click(function(){
+        $(".sub-sidenav-content-project").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-email").click(function(){
+        $(".sub-sidenav-content-email").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-event").click(function(){
+        $(".sub-sidenav-content-event").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-kanban").click(function(){
+        $(".sub-sidenav-content-kanban").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-social").click(function(){
+        $(".sub-sidenav-content-social").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-faq").click(function(){
+        $(".sub-sidenav-content-faq").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-landing").click(function(){
+        $(".sub-sidenav-content-landing").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-pricing").click(function(){
+        $(".sub-sidenav-content-pricing").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-errors").click(function(){
+        $(".sub-sidenav-content-errors").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-authentic").click(function(){
+        $(".sub-sidenav-content-authentic").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-simple").click(function(){
+        $(".sub-sidenav-content-simple").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-split").click(function(){
+        $(".sub-sidenav-content-split").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-card").click(function(){
+        $(".sub-sidenav-content-card").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-layout").click(function(){
+        $(".sub-sidenav-content-layout").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-form").click(function(){
+        $(".sub-sidenav-content-form").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-basic").click(function(){
+        $(".sub-sidenav-content-basic").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-advance").click(function(){
+        $(".sub-sidenav-content-advance").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-i-cones").click(function(){
+        $(".sub-sidenav-content-i-cones").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-table").click(function(){
+        $(".sub-sidenav-content-table").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-chart").click(function(){
+        $(".sub-sidenav-content-chart").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-component").click(function(){
+        $(".sub-sidenav-content-component").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-carousel").click(function(){
+        $(".sub-sidenav-content-carousel").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-nav-tab").click(function(){
+        $(".sub-sidenav-content-nav-tab").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-picture").click(function(){
+        $(".sub-sidenav-content-picture").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-untilies").click(function(){
+        $(".sub-sidenav-content-untilies").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-level").click(function(){
+        $(".sub-sidenav-content-level").fadeToggle();
+    })
+})
+
+
+$(document).ready(function(){
+    $(".sidenav-content-two").click(function(){
+        $(".sub-sidenav-content-two").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-three").click(function(){
+        $(".sub-sidenav-content-three").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-itam-four").click(function(){
+        $(".sub-sidenav-content-itam-four").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-itam-seven").click(function(){
+        $(".sub-sidenav-content-itam-seven").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-four").click(function(){
+        $(".sub-sidenav-content-four").fadeToggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".sidenav-content-customizaion").click(function(){
+        $(".sub-sidenav-content-customizaion").fadeToggle();
+    })
+})
+
+
+// model 3
+$(document).ready(function(){
+    $(".iconesImage").click(function(){
+        $(".sub-menu-wrap").fadeToggle();
+    })
+})
+
+// model 2
+$(document).ready(function(){
+    $(".sub-icone-2").click(function(){
+        $(".sub-menu-wrap-1").fadeToggle();
+    })
+})
+
+// model 1
+$(document).ready(function(){
+  $(".sub-icone-1").click(function(){
+      $(".wrapper").fadeToggle();
+  })
+})
+
+{/* <script type="text/javascript"> */}
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+  
+        function drawChart() {
+          var data = google.visualization.arrayToDataTable([
+            ['Year', 'Sales', 'Expenses'],
+            ['2004',  1000,      400],
+            ['2005',  1170,      460],
+            ['2006',  660,       1120],
+            ['2007',  1030,      540]
+          ]);
+  
+          var options = {
+            title: 'Company Performance',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+          };
+  
+          var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+  
+          chart.draw(data, options);
+        }
+    //   </script>
+
+
+
+
+
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+  var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+  var yValues = [55, 49, 44, 24, 15];
+  var barColors = ["red", "green","blue","orange","brown"];
+  
+  new Chart("myChart", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      title: {
+        display: true,
+        text: "World Wine Production 2018"
+      }
+    }
+  });
+
+
+  var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+  var yValues = [55, 49, 44, 24, 15];
+  var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+  ];
+  
+  new Chart("MeraChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+      }
+    }
+  });
+
+
+  const aValues = [50,60,70,80,90,100,110,120,130,140,150];
+  const bValues = [7,8,8,9,9,9,10,11,14,14,15];
+  
+  new Chart("matChart", {
+    type: "line",
+    data: {
+      labels: aValues,
+      datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "rgba(0,0,255,0.1)",
+        data: bValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        yAxes: [{ticks: {min: 6, max:16}}],
+      }
+    }
+  });
+
+
+
+  var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+  var yValues = [55, 49, 44, 24, 15];
+  var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+  ];
+  
+  new Chart("SabkaChart", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+      }
+    }
+  });
+
+
+  google.charts.load('current', {
+    'packages':['geochart'],
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap);
+
+  function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
+      ['Country', 'Popularity'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 500],
+      ['France', 600],
+      ['RU', 700]
+    ]);
+
+    var options = {};
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options);
+  }
+
+
+  var xyValues = [
+    {x:50, y:7},
+    {x:60, y:8},
+    {x:70, y:8},
+    {x:80, y:9},
+    {x:90, y:9},
+    {x:100, y:9},
+    {x:110, y:10},
+    {x:120, y:11},
+    {x:130, y:14},
+    {x:140, y:14},
+    {x:150, y:15}
+  ];
+  
+  new Chart("footerChart1", {
+    type: "scatter",
+    data: {
+      datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(0,0,255)",
+        data: xyValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        xAxes: [{ticks: {min: 40, max:160}}],
+        yAxes: [{ticks: {min: 6, max:16}}],
+      }
+    }
+  });
+
+  
+
+
+  const zValues = [100,200,300,400,500,600,700,800,900,1000];
+
+  new Chart("footerChart", {
+    type: "line",
+    data: {
+      labels: zValues,
+      datasets: [{ 
+        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+        borderColor: "red",
+        fill: false
+      }, { 
+        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+        borderColor: "green",
+        fill: false
+      }, { 
+        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+        borderColor: "blue",
+        fill: false
+      }]
+    },
+    options: {
+      legend: {display: false}
+    }
+  });
